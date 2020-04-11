1d3765f7c5200ce2fc56ee4864a21a4f415227d8
Adam Sharp <adam@sharplet.me>
2020-04-11 10:05:16 -0400

    Add library product

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
   name: "GTMAppAuth",
+  products: [
+    .library(name: "GTMAppAuth", targets: ["GTMAppAuth"]),
+  ],
   dependencies: [
     .package(url: "https://github.com/sharplet/AppAuth-iOS.git", .branch("swiftpm")),
     .package(url: "https://github.com/sharplet/gtm-session-fetcher.git", .branch("swiftpm")),
